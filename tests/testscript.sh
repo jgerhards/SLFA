590fd0a52b6d884ceaf0dc82d31f70ab23c46390
Gregor Santner <gsantner@mailbox.org>
2018-03-15 14:25:04 +0100

    Remove autogenerated files ; Update build files
    * Remove autogenerated build files
    * Update buildscript
    * Update tests to not fail on different working directory

---
+++ tests/testscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECTDIR="$(dirname "$SCRIPTDIR")"
+TESTCONF="$PROJECTDIR/build/testconfig.properties"
+TESTLOG_A="$PROJECTDIR/build/test-after.log"
+TESTLOG_B="$PROJECTDIR/build/test-before.log"
+
+mkdir -p "$(dirname "$TESTCONF")"
+
+$SCRIPTDIR/simple_test.sh
